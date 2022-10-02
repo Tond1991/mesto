79827918b0e7c9f94bc353420f45dc4b7f7851a9
Maksim Filatov <gordan2010@yandex.ru>
2022-10-02 20:21:50 +0300

    Верстка + Адаптив

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+const openModalBtn = document.querySelector(".profile__editing");
+const modal = document.querySelector(".modal");
+
+openModalBtn.addEventListener("click", function(event) {
+modal.classList.add("modal_active");
+});
+
+
+
+
+
+
+
