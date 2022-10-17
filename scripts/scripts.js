88a47f57fc4cdc6ae6db30f7c1b4aaec53e4c654
Maksim Filatov <gordan2010@yandex.ru>
2022-10-18 00:23:15 +0300

    ПР 5

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,46 +1,148 @@
-const openModalBtn = document.querySelector(".profile__editing");
+const editProfileBtn = document.querySelector(".profile__editing");
 const modal = document.querySelector(".modal");
-const modalContent = modal.querySelector(".modal__content");
-const closeModalBtn = modal.querySelector(".modal__close");
+const addNewCardBtn = document.querySelector(".profile__button");
+const editProfileModal = document.querySelector(".modal_type_profile");
+const addNewCardModal = document.querySelector(".modal_type_add-new-card");
 const profileContent = document.querySelector(".profile__content");
 const nameText = profileContent.querySelector(".profile__name");
 const professionText = profileContent.querySelector(".profile__profession");
-const nameInpt = modal.querySelector(".modal__input_form_name");
-const professionInpt = modal.querySelector(".modal__input_form_profession");
-const modalForm = modal.querySelector(".modal__form");
-let likeBtn = document.querySelectorAll(".photo__icon");
+const nameInpt = editProfileModal.querySelector(".modal__input_form_name");
+const professionInpt = editProfileModal.querySelector(".modal__input_form_profession");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const addNewCardForm = addNewCardModal.querySelector(".modal__form");
+const mestoInpt = addNewCardModal.querySelector(".modal__input_form_mesto");
+const urlInpt = addNewCardModal.querySelector(".modal__input_form_url");
+const photoCardsTemplate = document.querySelector(".photo-cards");
+const photoTemplate = document.querySelector("#photo-item-template").content.querySelector(".photo__cell");
+const photoModal = document.querySelector(".modal_type_photo");
 
 
-function openPop (event) {
-    modal.classList.add("modal_active");
-    nameInpt.value = "Жак-Ив Кусто";
-    professionInpt.value = "Исследователь океана";
+function openModal (modal, event) {
+        modal.classList.add("modal_active");
 };
 
-function saveSubmitPop (event) {
+function openProfileModal (event) {
+    nameInpt.value = "Жак-Ив Кусто";
+    professionInpt.value = "Исследователь океана";
+    openModal(editProfileModal, event)
+ };
+ 
+ function openAddNewCard(event) {
+   openModal(addNewCardModal, event)
+ };
+
+
+ function openPhotoModal (photoData, event) {
+    document.querySelector(".modal__photo").src = photoData.link;
+    document.querySelector(".modal__photo").alt = photoData.name;
+    document.querySelector(".modal__caption").textContent = photoData.name;
+    openModal(photoModal, event);
+ };
+
+function saveSubmitPop(event) {
     nameText.textContent = nameInpt.value;
     professionText.textContent = professionInpt.value;
     event.preventDefault();
-    modal.classList.remove("modal_active");
-
+    editProfileModal.classList.remove("modal_active");
 };
 
-function closePop (event) {
-    if(!modalContent.contains(event.target) || event.target === closeModalBtn) {
+function closeModal (modal, event) {
+        
+       if (!modal.querySelector(".modal__content").contains(event.target) || event.target === modal.querySelector(".modal__close")) {
         modal.classList.remove("modal_active");
     }
 };
 
-openModalBtn.addEventListener("click", openPop);
-modal.addEventListener("click", closePop);
-modalForm.addEventListener("submit", saveSubmitPop); 
+function closeProfileModal (event) {
+    closeModal(editProfileModal, event)
+ };
+
+ function closeNewCardModal  (event) {
+    closeModal(addNewCardModal, event)
+ };
+
+ function closePhotoModal (event) {
+    closeModal(photoModal, event)
+ };
+
+ const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    },
+];
 
 
-for (let index = 0; index < likeBtn.length; index++) {
-    likeBtn[index].addEventListener("click", function () {
-        likeBtn[index].classList.toggle("photo__icon_black");
-    });
+const photoCards = (element) => {
+    const elementPhotoTemplate = photoTemplate.cloneNode(true);
+    const removePhotoCardBtn = elementPhotoTemplate.querySelector(".photo__delete");
+    
+    elementPhotoTemplate.querySelector(".photo__image").src = element.link;
+   elementPhotoTemplate.querySelector(".photo__image").alt = element.name;
+   elementPhotoTemplate.querySelector(".photo__title").textContent = element.name;
+   elementPhotoTemplate.querySelector(".photo__icon").addEventListener("click", function (event) {
+        event.target.classList.toggle("photo__icon_black");
+    })
+ 
+    elementPhotoTemplate.querySelector(".photo__image").addEventListener("click", (event) => openPhotoModal(element, event));
+    removePhotoCardBtn.addEventListener("click", () => removePhotoCard(elementPhotoTemplate));
+    photoCardsTemplate.prepend(elementPhotoTemplate);
 }
+
+initialCards.forEach(photoCards); 
+
+const saveCardSubmit = (event) => {
+    event.preventDefault();
+    
+    photoCards({
+        link: urlInpt.value,
+        name: mestoInpt.value
+    },);
+
+    addNewCardModal.classList.remove("modal_active");
+    clearInput();
+};
+
+const removePhotoCard = (element) => {
+    element.remove();
+}
+
+const clearInput = () => {
+    urlInpt.value = '',
+    mestoInpt.value = '';
+}
+
+
+
+editProfileBtn.addEventListener("click", openProfileModal);
+editProfileModal.addEventListener("click", closeProfileModal);
+addNewCardBtn.addEventListener("click", openAddNewCard);
+addNewCardModal.addEventListener("click", closeNewCardModal);
+photoModal.addEventListener("click", closePhotoModal);
+editProfileForm.addEventListener("submit", saveSubmitPop);
+addNewCardForm.addEventListener("submit", saveCardSubmit);
+
+
 
 
 
